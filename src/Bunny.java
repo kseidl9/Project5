d62c5c5997ec7dad75665580770d2208333aa47f
Katie Seidl <katie.seidl@comcast.net>
2019-06-04 13:18:39 -0700

    Editied Bunny

--- src/Bunny.java
+++ src/Bunny.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import processing.core.PImage;
 
-public class Bunny {
+import java.util.List;
+
+public class Bunny extends AbstractMobileEntity{
+
+    private int resourceLimit;
+    private int resourceCount;
+
+    public Bunny(String id, Point position,
+                 List<PImage> images, int resourceLimit, int resourceCount,
+                 int actionPeriod, int animationPeriod)
+    {
+        super(id, position, images, actionPeriod, animationPeriod);
+        this.resourceLimit = resourceLimit;
+        this.resourceCount = resourceCount;    }
+
+    @Override
+    public Point nextPosition(WorldModel world, Point destPos) {
+        return null;
+    }
+
+    @Override
+    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
+
+    }
 }
